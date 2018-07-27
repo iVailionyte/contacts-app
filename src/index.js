69ec88a37ca546b0815ed19ab0234c4a51eb5d21
Indre <indre.vailionyte@moovel.com>
2018-07-27 08:55:33 +0200

    setup babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log('Hello, world!');
+import React from 'react';
+import { render } from 'react-dom';
+
+/*
+Smart Component
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello
+      <div>
+    )
+  }
+}
+
+
+! DO NOT USE !
+Function Component
+
+function App() {
+  return (
+    <div>
+      Hello
+    <div>
+  )
+}
+*/
+
+const App = () => (
+  <div>
+    Hello
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
