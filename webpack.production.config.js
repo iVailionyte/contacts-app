2fe5d26c47a8c546c36150f87a4a8bbdb8c7b192
Indre <indre.vailionyte@moovel.com>
2018-08-01 09:53:56 +0200

    setup webpack-dev-server

---
+++ webpack.production.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
