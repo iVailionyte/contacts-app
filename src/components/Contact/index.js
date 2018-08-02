216c6d403ab5cd13c99d24fc681b391358d3ce2f
Indre <indre.vailionyte@moovel.com>
2018-08-02 08:23:57 +0200

    fix #7 check for properties

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,10 +4,16 @@ const Contact = (props) => (
   <div>
     <h1>{props.firstname} {props.lastname}</h1>
     <address>{props.street}<br />{props.zip} {props.city}<br />{props.country}</address>
-    <ul>
-      <li>{props.phone}</li>
-      <li>{props.email}</li>
-    </ul>
+    {props.phone || props.email && (
+      <ul>
+        {props.phone && (
+          <li>{props.phone}</li>
+        )}
+        {props.email && (
+          <li>{props.email}</li>
+        )}
+      </ul>
+    )}
   </div>
 );
 
