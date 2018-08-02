ce2283f2853981f50a8e519681a6fb139c6e3b83
iVailionyte <indre.vailionyte@gmail.com>
2018-08-02 10:43:43 +0200

    fix #11 add layout component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,18 +1,25 @@
+import PropTypes from 'prop-types';
 import React from 'react';
-import Contacts from '../Contacts';
-import contacts from '../../data/contacts';
 import Header from '../Header';
 import Footer from '../Footer';
 import Main from '../Main';
 
-const App = () => (
+const Layout = (props) => (
   <div>
     <Header>Navigation</Header>
     <Main>
-      <Contacts contacts={contacts} />
+      {props.children}
     </Main>
     <Footer>Copyright information</Footer>
   </div>
 );
 
-export default App;
+Layout.propTypes = {
+  children: PropTypes.node,
+};
+
+Layout.defaultProps = {
+  children: null,
+};
+
+export default Layout;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
+import Contacts from './components/Contacts';
+import Layout from './components/Layout';
+import contacts from './data/contacts';
 
-/*
-Smart Component
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello
-      <div>
-    )
-  }
-}
-
-
-! DO NOT USE !
-Function Component
-
-function App() {
-  return (
-    <div>
-      Hello
-    <div>
-  )
-}
-*/
-
-
-
-render(<App />, document.getElementById('root'));
+render(<Layout><Contacts contacts={contacts} /></Layout>, document.getElementById('root'));
