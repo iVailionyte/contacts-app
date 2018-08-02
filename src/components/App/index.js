2862ee5faceda9eb35e8927b1166ab8f3542d7a5
Indre <indre.vailionyte@moovel.com>
2018-08-02 08:13:30 +0200

    separated data from app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,42 +1,6 @@
 import React from 'react';
-import uuidv4 from 'uuid/v4';
 import Contacts from '../Contacts';
-
-const contacts = [
-  {
-    id: uuidv4(),
-    firstname: 'Jane',
-    lastname: 'Janny',
-    street: 'Street',
-    zip: '165',
-    city: 'Stuttgart',
-    country: 'Germany',
-    phone: '1414',
-    email: 'ahsbsdjd@jskw',
-  },
-  {
-    id: uuidv4(),
-    firstname: 'John',
-    lastname: 'Janny',
-    street: 'Street',
-    zip: '165',
-    city: 'Stuttgart',
-    country: 'Germany',
-    phone: '1414',
-    email: 'ahsbsdjd@jskw',
-  },
-  {
-    id: uuidv4(),
-    firstname: 'John',
-    lastname: 'Janny',
-    street: 'Street',
-    zip: '165',
-    city: 'Stuttgart',
-    country: 'Germany',
-    phone: '1414',
-    email: 'ahsbsdjd@jskw',
-  },
-];
+import contacts from '../../data/contacts';
 
 const App = () => (
   <div>
---
+++ src/data/contacts.js
@@ -0,0 +1,40 @@
+import uuidv4 from 'uuid/v4';
+import Contacts from '../components/Contacts';
+
+const contacts = [
+  {
+    id: uuidv4(),
+    firstname: 'Jane',
+    lastname: 'Janny',
+    street: 'Street',
+    zip: '165',
+    city: 'Stuttgart',
+    country: 'Germany',
+    phone: '1414',
+    email: 'ahsbsdjd@jskw',
+  },
+  {
+    id: uuidv4(),
+    firstname: 'John',
+    lastname: 'Janny',
+    street: 'Street',
+    zip: '165',
+    city: 'Stuttgart',
+    country: 'Germany',
+    phone: '1414',
+    email: 'ahsbsdjd@jskw',
+  },
+  {
+    id: uuidv4(),
+    firstname: 'John',
+    lastname: 'Janny',
+    street: 'Street',
+    zip: '165',
+    city: 'Stuttgart',
+    country: 'Germany',
+    phone: '1414',
+    email: 'ahsbsdjd@jskw',
+  },
+];
+
+export default contacts;
