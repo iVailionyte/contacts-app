0fdfecbbe0b551b5e4281c0ea2586ffb0c3bdde1
Indre <indre.vailionyte@moovel.com>
2018-08-02 08:56:34 +0200

    fix #9 header component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Contacts from '../Contacts';
 import contacts from '../../data/contacts';
+import Header from '../Header';
 
 const App = () => (
   <div>
+    <Header>Navigation</Header>
     Hello
     <Contacts contacts={contacts} />
   </div>
